b5a7b544653d0366ed4cbe2425a6808bb01f78eb
mariosk <marios.koulakis@gmail.com>
2020-01-26 14:48:29 +0100

    Lyapunov stability.

--- phase_space_plotting.py
+++ phase_space_plotting.py
@@ -3,12 +3,14 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 
-def plot_traj(x0, dx_dt, fw=1, bw=1, n_time_samples=100, ax=None):
+def plot_traj(x0, dx_dt, fw=0, bw=0, n_time_samples=100, ax=None, color=None):
+    if color is None:
+        color='red'
     xs = odeint(dx_dt, x0, np.linspace(0, fw, n_time_samples))
-    (ax if ax else plt).plot(xs[:, 0], xs[:, 1], 'r-', linewidth=1)
+    (ax if ax else plt).plot(xs[:, 0], xs[:, 1], linewidth=1, color=color)
     
-    xs = odeint(dx_dt, x0, np.linspace(0, bw, n_time_samples))
-    (ax if ax else plt).plot(xs[:, 0], xs[:, 1], 'r-', linewidth=1)
+    xs = odeint(dx_dt, x0, np.linspace(0, -bw, n_time_samples))
+    (ax if ax else plt).plot(xs[:, 0], xs[:, 1], linewidth=1, color=color)
     
 
 def plot_field(dx_dt, xlim, ylim, scale=100, ax=None, polar=False):
@@ -25,9 +27,9 @@ def plot_phase_portrait(
     title=None,
     ic_x = np.linspace(*[-3, 3], 9),
     ic_y = np.linspace(*[-3, 3], 9),
-    linewidth=1):
+    linewidth=1,
+    scale=None):
 
-    plt.figure(figsize=(6, 6))
     for r in ic_x:
         for s in ic_y:
             x0 = [r, s]
@@ -35,12 +37,9 @@ def plot_phase_portrait(
             plt.plot(xs[:, 0], xs[:, 1], 'r-', linewidth=linewidth)
 
     X, Y = np.mgrid[xlim[0]:xlim[1]:15j, ylim[0]:ylim[1]:15j]
-    u = a*X + b*Y
-    v = c*X + d*Y
-    plt.quiver(X, Y, u, v, color='b')
+    u, v = dx_dt([X, Y], None)
+    plt.quiver(X, Y, u, v, color='b', scale=scale)
 
     plt.title(title)
     plt.xlim(np.array(xlim) * 1.1)
     plt.ylim(np.array(ylim) * 1.1)
-
-    plt.show()
